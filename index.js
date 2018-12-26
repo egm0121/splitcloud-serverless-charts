f80737732d4f4f5f0cb2ef1a6c1d9e5cc294ba9a
Giulio Dellorbo <egm0121@gmail.com>
2018-12-25 21:08:40 -0500

    added fetch from GA Reporting api and hydrate each item with track details from SC api

--- index.js
+++ index.js
@@ -1,50 +1,113 @@
-'use strict';
+const { google } = require('googleapis');
+const axios = require('axios');
+const fs = require('fs');
+const privatekey = require('./key/splitcloud-lambda-04bda8c26386.json');
+const soundcloudkey = require('./key/soundcloud_key.json');
 
-const {google} = require('googleapis');
-const sampleClient = require('../sampleclient');
 
-const analyticsreporting = google.analyticsreporting({
-  version: 'v4',
-  auth: sampleClient.oAuth2Client,
-});
+const SC_API_ENDPOINT = 'api.soundcloud.com';
+function generateAuthClient() {
+  const jwtClient = new google.auth.JWT(privatekey.client_email, null, privatekey.private_key, [
+    'https://www.googleapis.com/auth/analytics.readonly',
+  ]);
+  // authenticate request
+  return jwtClient.authorize().then(resToken => {
+    console.log('Successfully connected!', resToken);
+    const oauth2Client = new google.auth.OAuth2();
+    oauth2Client.setCredentials({
+      access_token: resToken.access_token,
+    });
+    return oauth2Client;
+  });
+}
 
-async function runSample() {
+async function runSample(authClient) {
+  const analyticsreporting = google.analyticsreporting({
+    version: 'v4',
+    auth: authClient,
+  });
+
   const res = await analyticsreporting.reports.batchGet({
     requestBody: {
       reportRequests: [
         {
-          viewId: '65704806',
+          viewId: '152777884',
           dateRanges: [
             {
-              startDate: '2018-03-17',
-              endDate: '2018-03-24',
-            },
-            {
-              startDate: '14daysAgo',
-              endDate: '7daysAgo',
+              startDate: '7daysAgo',
+              endDate: '0daysAgo',
             },
           ],
           metrics: [
             {
-              expression: 'ga:users',
+              expression: 'ga:totalEvents',
+            },
+            {
+              expression: 'ga:uniqueEvents',
             },
           ],
+          dimensions: [
+            {
+              name: 'ga:dimension1',
+            },
+          ],
+          orderBys: [
+            { fieldName: 'ga:uniqueEvents', sortOrder: 'DESCENDING' },
+            { fieldName: 'ga:totalEvents', sortOrder: 'DESCENDING' },
+          ],
+          pageSize: '50',
         },
       ],
     },
   });
-  console.log(res.data);
-  return res.data;
+  return res;
+}
+async function fetchScTrackById(trackId){
+  const trackUrl = `http://${SC_API_ENDPOINT}/tracks/${trackId}?client_id=${soundcloudkey.SC_CLIENT_ID}`;
+  return axios({ method: 'GET', url: trackUrl, timeout: 2000 });
+}
+async function hydrateSoundcloudTracks(trackList){
+  const finalTracks = {};
+  return trackList
+    .map(track => {
+      const resolveTrack = Object.assign({}, track);
+      resolveTrack.fetch = () => fetchScTrackById(track.id);
+      return resolveTrack;
+    })
+    .slice(0,10)
+    .reduce((prevPromise, nextTrackObj,idx,initList) => {
+      const currTrackObj = initList[idx-1];
+      return prevPromise.then(resp => {
+        if (resp) {
+          console.log('hydrate track', currTrackObj.id, 'sc id', resp.data.id);
+          finalTracks[currTrackObj.id] = Object.assign({}, currTrackObj, resp.data);
+        }
+        return nextTrackObj.fetch();
+      }).catch(err => {
+          console.warn('sc track ' + currTrackObj.id + ' retrival failed', err.message);
+          return Promise.resolve();
+      });
+    }, Promise.resolve())
+    .then(() => Object.values(finalTracks));
+}
+function extractResponseRows(response) {
+  return response.data.reports[0].data.rows.map(row => {
+    const [totalPlays, uniquePlays] = row.metrics[0].values;
+    return {
+      id: row.dimensions[0],
+      debug_id: row.dimensions[0],
+      splitcloud_total_plays: totalPlays,
+      splitcloud_unique_plays: uniquePlays,
+    };
+  });
 }
 
-const scopes = ['https://www.googleapis.com/auth/analytics'];
-sampleClient
-  .authenticate(scopes)
+generateAuthClient()
   .then(runSample)
-  .catch(console.error);
+  .then(extractResponseRows)
+  .then(hydrateSoundcloudTracks)
+  .then(tracks => {
+    console.log('FINAL TRACKS', tracks);
+    fs.writeFileSync('./top_splitcloud_tracks.json',JSON.stringify(tracks));
+  })
 
-// export functions for testing purposes
-module.exports = {
-  runSample,
-  client: sampleClient.oAuth2Client,
-};
