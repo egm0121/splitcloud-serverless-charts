a662c4b630a08fd16c58e3ef2c71eddb99182496
Giulio Dellorbo <giulio.dellorbo@8x8.com>
2020-07-09 17:00:49 -0400

    use internally owned sc track in ActiveStreamToken service

--- src/modules/activeStreamToken.js
+++ src/modules/activeStreamToken.js
@@ -7,7 +7,7 @@ const INITIAL_ACTIVE_TOKEN = availableStreamTokens[1].SC_CLIENT_ID;
 // const ACTIVE_TOKEN_S3_PATH = 'app/app_config.json';
 const ACTIVE_TOKEN_S3_PATH_V2 = 'app/app_config_v2.json';
 const MAX_USAGE_PER_DAY = 13000;
-const TEST_TRACK_URL = 'https://api.soundcloud.com/tracks/397263444/stream';
+const TEST_TRACK_URL = 'https://api.soundcloud.com/tracks/855191668/stream';
 
 async function getActiveToken() {
   let jsonData;
