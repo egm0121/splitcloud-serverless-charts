c97927a9af81e9ef3bd5243688ce7ad8b3ef18c1
Giulio Dellorbo <giulio.dellorbo@8x8.com>
2020-09-17 13:04:06 -0400

    feature do not suggest non unicode script matching results

--- src/modules/chartsService.js
+++ src/modules/chartsService.js
@@ -153,7 +153,7 @@ async function hydrateSoundcloudTracks(trackList, scApiToken) {
     .map(track => {
       const resolveTrack = Object.assign({}, track);
       resolveTrack.fetch = () => {
-        return fetchScTrackById(track.id, scApiToken).catch(err => {
+        return (track.id, scApiToken).catch(err => {
           console.warn(`sc track ${track.id} retrival failed`, err.message);
           return Promise.resolve();
         });
@@ -298,6 +298,39 @@ class ChartsService {
 
   fetchRelatedTracksById(id) {
     return fetchRelatedTracksById(id);
+  }
+
+  fetchScTrackById(id) {
+    return fetchScTrackById(id);
+  }
+
+  async fetchAllRelated(sourceTrackIds) {
+    const allRelatedReq = sourceTrackIds.map(trackId => {
+      try {
+        return this.fetchRelatedTracksById(trackId);
+      } catch (err) {
+        return { data: [] };
+      }
+    });
+    const responsesArr = await Promise.all(allRelatedReq);
+    // flatten all related tracks in one list
+    return responsesArr.reduce((finalList, resp) => {
+      const subsetRelated = resp.data;
+      finalList.push(...subsetRelated);
+      return finalList;
+    }, []);
+  }
+
+  async fetchScTrackList(trackList) {
+    const trackProms = trackList.map(id => {
+      try {
+        return this.fetchScTrackById(id);
+      } catch (err) {
+        return {};
+      }
+    });
+    const respArr = await Promise.all(trackProms);
+    return respArr.map(resp => resp.data);
   }
 
   getScTrendingChart() {
