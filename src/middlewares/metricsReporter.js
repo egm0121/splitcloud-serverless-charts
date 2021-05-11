76d70b7509ebb9ade145b16a97e049ec9ef401f6
Giulio Dellorbo <giulio.dellorbo@8x8.com>
2021-05-11 17:03:35 -0400

    small tweak

--- src/middlewares/metricsReporter.js
+++ src/middlewares/metricsReporter.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-param-reassign */
 const { metricScope } = require('aws-embedded-metrics');
 
-const metricsReporterMiddleware = (opts = {}) => (event, context, callback, next) => {
+const metricsReporterMiddleware = () => (event, context, callback, next) => {
   const decoratedHandler = metricScope(metrics => {
     context.metrics = metrics;
     return async () => next();
